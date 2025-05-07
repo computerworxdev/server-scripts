1bd0632cdc6e91b2868a6357f1124e20aed037e3
Jaime Lopez <jailop@protonmail.com>
2025-05-07 17:39:33 -0400

    sh functions

--- Rocky8-LAMP/functions/is_service_active.sh
+++ Rocky8-LAMP/functions/is_service_active.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 # pre-conditions
-. set_colors.sh
+. messages.sh
 
 is_service_running() {
     local service="$1"
     if systemctl is-active --quiet "$service"; then
-        echo -e "${COLOR_GREEN}$service is active${COLOR_NAN}"
+        success "$service is active"
         return 0
     else
-        echo -e "${COLOR_RED}$service is inactive${COLOR_NAN}"
+        alert "$service is inactive"
         return 1
     fi
 }
